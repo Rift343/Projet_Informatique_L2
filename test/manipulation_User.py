808b76a6ca9070e5152106cec48917abfae15718
Rift343 <matthieu.conrath@etu.umontpellier.fr>
2023-01-16 21:07:13 +0100

    Ajout du csv des utilisateur + début de la manipulation d'un utilisateur

---
+++ test/manipulation_User.py
@@ -0,0 +1,41 @@
+import csv
+import os
+
+littlePATH = "\\test"
+
+def lireCSV():
+    PATH = os.getcwd()
+    PATH = PATH+littlePATH+"\\testUser.csv"
+    with open(PATH,'r') as FILE:
+        lecture = csv.reader(FILE,delimiter=';')
+        ListeUSER=[]
+        for i in lecture:
+            ListeUSER.append(i)
+    return ListeUSER
+            
+
+print(lireCSV())
+
+def ajouterUser(Nom,password,email):
+    PATH = os.getcwd()
+    PATH = PATH+littlePATH+"\\testUser.csv"
+    listeUSER = lireCSV()
+    listeid=[]
+    for user in listeUSER:
+        listeid.append(user[0])
+        if (password==user[3] or email==user[2] ):
+            return False
+    
+    with open(PATH,'a',newline='') as FILE:
+        id = 1
+        while (id in listeid):
+            id = id +1
+        Ecriture=csv.writer(FILE,delimiter=';')   
+        Ecriture.writerow([id,Nom,email,password])      
+        
+    return True
+
+print(ajouterUser("Jaque","PassWord","Jaque@yahoo.com"))
+
+
+    
