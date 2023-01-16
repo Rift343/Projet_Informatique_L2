19ed91911664cefb61ab2a2a3c49c97f74dfea15
Rift343 <matthieu.conrath@etu.umontpellier.fr>
2023-01-16 13:37:36 +0100

    Commentaire pour la lecture d'un fichier csv

--- test/manipulationQuestion.py
+++ test/manipulationQuestion.py
@@ -4,14 +4,26 @@ import os
 def depuis_csv(ID_User):
     """
     Ouvre un fichier au format csv
+    Renvoie une liste de dictionnaire avec clé disponible:
+    ID=>id de la question (String)
+    Question=> Enoncé de la question (String)
+    ET => liste des étiquettes(liste de String)
+    REP => liste de réponses possible (liste de String)
+    BREP => liste des bonne réponse (liste de String)
     """    
     PATH = os.getcwd()
-    PATH = PATH+"\\test\question_"+str(ID_User)+".csv"
-    with open(PATH,'r') as FILE:
+    #On récupere le PATH jusqu'au répertoire de travails
+    PATH = PATH+"\\test\question_"+str(ID_User)+".csv"#Au modifié pour correspondre dès que les csv seront changés de répertoires
+    #On ajoute au PATH le chemin vers notre fichier 
+    with open(PATH,'r') as FILE:#Ouverture du fichier
         lecture=csv.reader(FILE,delimiter=';')
+        #Lecture du fichier csv avec pour délimiter ;
         ListeQuestion=[]
         for ligne in lecture:
             ListeQuestion.append(ligne)
+            #on récupère les listes dans lecture afin de les écrire dans une liste de liste
+        #Dans les lignes suivantes, l'on va récupérer les informations dans les différentes liste de listes
+        #afin d'écrire c'est informations dans une liste de dictionnaire
         ListeDicoQuestion=list()
         for ligne in ListeQuestion:
             listeET=[]
