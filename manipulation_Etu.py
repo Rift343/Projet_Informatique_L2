55ce12e870f33939590b6e71642fdf87037b5dc9
Rift343 <matthieu.conrath@etu.umontpellier.fr>
2023-02-06 16:01:56 +0100

    répatation d'une erreur

--- manipulation_Etu.py
+++ manipulation_Etu.py
@@ -42,8 +42,7 @@ def ajoutEtu(fichierCSV):
                 mdp = i[-1]
                 i.append(hashlib.sha256(mdp.encode()).hexdigest())
                 Ecriture.writerow(i)
-    PATH = os.getcwd()
-    fichierCSV = PATH +'/'+fichierCSV
+    
     os.remove(fichierCSV)
     return True
                 
